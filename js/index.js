87ad250599406b277a107cf24a53acec39c14316
razumeyka <33911265+razumeyka@users.noreply.github.com>
2018-02-22 20:27:53 +0200

    sending form

--- js/index.js
+++ js/index.js
@@ -19,3 +19,27 @@ $( '.burger' ).click( function() {
     $( ".burger" ).removeClass( "active" );
   }
 });
+
+$(function(){
+   $('#contact-form button[type=button]').click(sendForm); 
+});
+
+function sendForm(ev) {
+    const form  = document.getElementsByTagName('form')[0];
+    if (form.checkValidity()) {
+        ev.preventDefault();
+        $.ajax({
+            url: "https://formspree.io/anastasiya.razumeyko@gmail.com", 
+            method: "POST",
+            data: {
+                name: $('#name').val(),
+                email: $('#email').val(),
+                feedback: $('#number').val(),
+                order: $('#order').val()
+            },
+            dataType: "json"
+        })
+         .done( () => $('#thank-dialog').attr('open', 'open') )
+         .fail( () => $('#error-dialog').attr('open', 'open') );   
+    }
+}
